62d165b9b311e64c23236df4c1a210edaec6c8e0
Mehul <318097@gmail.com>
2019-11-07 12:56:38 +0530

    Add: App setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Notes from './components/Notes';
+import AddNote from './components/AddNote';
 
-export default App;
+const App = () => (
+  <div>
+    <AddNote />
+    <Notes />
+  </div>
+);
+
+const mapStateToProps = state => ({ notes: state.notes.notes });
+
+export default connect(mapStateToProps)(App);
