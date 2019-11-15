af0127b3f83fbbae8dc1a86fdda327e4b6db50da
Mehul <318097@gmail.com>
2019-11-15 11:10:33 +0530

    Add: Router navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Route, Switch } from 'react-router-dom';
 
 import { auth } from './firebase';
 
@@ -8,35 +8,43 @@ import './App.scss';
 
 import Header from './components/Header';
 import Notes from './components/Notes';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
 
 import { setAddNoteModalVisibility, setSession } from './store/actions';
 
-const App = ({ location, dispatch }) => {
+const App = ({ location, history, dispatch, session }) => {
+  // const queryString = new URLSearchParams(location.search);
+  // const mode = queryString.get('mode');
+  // if (mode === 'add') {
+  //   dispatch(setAddNoteModalVisibility(true));
+  // }
+
   useEffect(() => {
     auth.onAuthStateChanged(user => {
       if (user) {
-        const { displayName, email, photoURL, uid } = user;
+        console.log(user);
+        const { displayName: name, email, photoURL, uid } = user;
         dispatch(setSession({
-          displayName, email, photoURL, uid
+          name, email, photoURL, uid
         }));
+        history.push('/home');
       }
     });
-
-    const queryString = new URLSearchParams(location.search);
-    const mode = queryString.get('mode');
-    if (mode === 'add') {
-      dispatch(setAddNoteModalVisibility(true));
-    }
   }, []);
 
   return (
     <div>
       <Header />
-      <Notes />
+      <Switch>
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/home" exact component={Notes} />
+      </Switch>
     </div>
   );
 }
 
-const mapStateToProps = state => ({ notes: state.notes });
+const mapStateToProps = ({ notes, session }) => ({ notes, session });
 
 export default withRouter(connect(mapStateToProps)(App));
