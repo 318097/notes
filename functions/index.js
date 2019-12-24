098611a03a3453ccf210b6913fd8939d0a850069
Mehul <318097@gmail.com>
2019-12-24 12:50:41 +0530

    Add: Home icon

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+// admin.initializeApp({
+//   apiKey: process.env.FIREBASE_API_KEY || 'AIzaSyBTfH9en075CKmOwoJuJbTCyqBforggU7g',
+//   authDomain: "notes-5211e.firebaseapp.com",
+//   databaseURL: "https://notes-5211e.firebaseio.com",
+//   projectId: "notes-5211e",
+//   storageBucket: "",
+//   messagingSenderId: "399043506173",
+//   appId: "1:399043506173:web:41f8346d4bc3c5e0"
+// });
+// const firestore = admin.firestore();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +17,14 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest(async (request, response) => {
   response.send("Hello from Firebase!");
 });
+
+// exports.test = functions.https.onRequest(async (request, response) => {
+//   let result = await firestore
+//     .collection('notes')
+//     .get();
+
+//   result = result.map(doc => doc.doc());
+
+//   console.log('Firestore result: ', result);
+//   response.send(result);
+// });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import styled from 'styled-components';
-import { connect } from 'react-redux';
-import { Spin, Icon, Divider, Popover } from 'antd';
-import { withRouter, Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { Spin, Icon, Divider, Popover } from "antd";
+import { withRouter, Link } from "react-router-dom";
 
-import { auth } from '../firebase';
-import { setSession, toggleSettingsDrawer } from '../store/actions';
+import { auth } from "../firebase";
+import { setSession, toggleSettingsDrawer } from "../store/actions";
 
-import AddNote from './AddNote';
-import { StyledIcon, ProfileIcon } from '../styled';
+import AddNote from "./AddNote";
+import { StyledIcon, ProfileIcon } from "../styled";
 
 const antIcon = <Icon type="loading" spin />;
 
@@ -22,7 +22,7 @@ const Container = styled.header`
   top: 0;
   background: inherit;
   z-index: 10;
-  h3{
+  h3 {
     flex: 1 1 auto;
     margin: 0;
     margin-left: 10px;
@@ -32,7 +32,7 @@ const Container = styled.header`
     a {
       font-family: LuckiestGuy;
       color: #424242;
-      & > span{
+      & > span {
         font-family: LuckiestGuy;
         text-decoration: underline;
         font-size: 150%;
@@ -40,79 +40,84 @@ const Container = styled.header`
       }
     }
   }
-  .controls{
+  .controls {
     flex: 0 0 auto;
     display: flex;
     align-items: center;
   }
-`
+`;
 const UserInfo = styled.div`
-display: flex;
-height: 24px;
-background: #dcdcdc;
-padding: 0px 10px;
-margin: 0;
-margin-right: 6px;
-border-radius: 20px;
-position: relative;
-.username{
+  display: flex;
+  height: 24px;
+  background: #dcdcdc;
+  padding: 0px 10px;
   margin: 0;
-  margin-right: 9px;
-  padding: 0 2px;
+  margin-right: 6px;
+  border-radius: 20px;
   position: relative;
-  top: 2px;
-  font-size: 13px;
-  font-weight: bold;
-  font-family: Saira;
-}
-`
+  .username {
+    margin: 0;
+    margin-right: 9px;
+    padding: 0 2px;
+    position: relative;
+    top: 2px;
+    font-size: 13px;
+    font-weight: bold;
+    font-family: Saira;
+  }
+`;
 
 const Header = ({ history, dispatch, appLoading, session }) => {
   const signOut = async () => {
     await auth.signOut();
     dispatch(setSession(null));
-    return history.push('/signin');
+    return history.push("/signin");
   };
 
   return (
     <Container>
       <h3>
         <Link to="/home">
-          <span>N</span>otes{' '}{appLoading && <Spin indicator={antIcon} />}
+          <span>N</span>otes {appLoading && <Spin indicator={antIcon} />}
         </Link>
       </h3>
-      {
-        session && (
-          <div className="controls">
-            <AddNote />
-            <Popover placement="bottom" content="Upload">
-              <StyledIcon type="upload" onClick={() => history.push('/upload')} />
-            </Popover>
-            <Divider style={{ background: 'black', 'position': 'relative', top: '4px' }} type="vertical" />
-            <UserInfo>
-              <div className="username">
-                {session.name}
-              </div>
-              <ProfileIcon>
-                {
-                  session.photoURL ?
-                    <img src={session.photoURL} alt="Profile pic" /> :
-                    <StyledIcon type="user" />
-                }
-              </ProfileIcon>
-            </UserInfo>
-            <Popover placement="bottom" content="Settings">
-              <StyledIcon type="setting" onClick={() => dispatch(toggleSettingsDrawer(true))} />
-            </Popover>
-            <Popover placement="bottom" content="Logout">
-              <StyledIcon type="logout" onClick={signOut} />
-            </Popover>
-          </div>
-        )
-      }
+      {session && (
+        <div className="controls">
+          <Popover placement="bottom" content="Home">
+            <StyledIcon type="home" onClick={() => history.push("/home")} />
+          </Popover>
+          <AddNote />
+          <Popover placement="bottom" content="Upload">
+            <StyledIcon type="upload" onClick={() => history.push("/upload")} />
+          </Popover>
+          <Divider
+            style={{ background: "black", position: "relative", top: "4px" }}
+            type="vertical"
+          />
+          <UserInfo>
+            <div className="username">{session.name}</div>
+            <ProfileIcon>
+              {session.photoURL ? (
+                <img src={session.photoURL} alt="Profile pic" />
+              ) : (
+                <StyledIcon type="user" />
+              )}
+            </ProfileIcon>
+          </UserInfo>
+          <Popover placement="bottom" content="Settings">
+            <StyledIcon
+              type="setting"
+              onClick={() => dispatch(toggleSettingsDrawer(true))}
+            />
+          </Popover>
+          <Popover placement="bottom" content="Logout">
+            <StyledIcon type="logout" onClick={signOut} />
+          </Popover>
+        </div>
+      )}
     </Container>
   );
-}
+};
 
 const mapStateToProps = ({ appLoading, session }) => ({ appLoading, session });
 
