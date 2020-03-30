adf8f0cf68d48d9acac63c3e67ce75bd0708643a
Mehul <318097@gmail.com>
2020-03-30 21:20:50 +0530

    Update: Filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,47 +1,78 @@
 import React from "react";
-import { Input, Select } from "antd";
+import { Input, Select, Button } from "antd";
 import { connect } from "react-redux";
 import { setFilter } from "../store/actions";
 
 const { Search } = Input;
 const { Option } = Select;
 
-const status = ["ALL", "DRAFT", "READY", "POSTED"];
-const socialStatus = ["NONE", "DRAFT", "READY", "POSTED"];
+const status = [
+  { label: "ALL", value: "" },
+  { label: "DRAFT", value: "DRAFT" },
+  { label: "READY", value: "READY" },
+  { label: "POSTED", value: "POSTED" }
+];
+const socialStatus = [
+  { label: "NONE", value: "" },
+  { label: "READY", value: "READY" },
+  { label: "POSTED", value: "POSTED" }
+];
+
+const validateFilters = ({ socialStatus, status, search, tags = [] } = {}) =>
+  socialStatus || status || search || tags.length;
 
 const Filters = ({ dispatch, filters, notes, meta, tags }) => {
-  const setFilterValues = (key, value) => dispatch(setFilter({ [key]: value }));
+  const setFilterValues = filter => dispatch(setFilter({ ...filter }));
 
   return (
     <div
       className="flex center align-center"
       style={{ flexShrink: 0, paddingBottom: "12px" }}
     >
+      {validateFilters(filters) ? (
+        <Button
+          type="dashed"
+          onClick={() =>
+            setFilterValues({
+              tags: [],
+              socialStatus: "",
+              status: "",
+              search: ""
+            })
+          }
+        >
+          Clear
+        </Button>
+      ) : null}
       <Search
         allowClear
         className="input-width"
         placeholder="Search..."
         defaultValue={filters.search}
-        onSearch={value => setFilterValues("search", value)}
+        onSearch={value => setFilterValues({ search: value })}
       />
       <Select
         className="input-width"
         placeholder="Post Status"
         value={filters.status}
-        onChange={value => setFilterValues("status", value)}
+        onChange={value => setFilterValues({ status: value })}
       >
-        {status.map(state => (
-          <Option key={state}>{state}</Option>
+        {status.map(({ label, value }) => (
+          <Option key={value} value={value}>
+            {label}
+          </Option>
         ))}
       </Select>
       <Select
         className="input-width"
         placeholder="Social Status"
         value={filters.socialStatus}
-        onChange={value => setFilterValues("socialStatus", value)}
+        onChange={value => setFilterValues({ socialStatus: value })}
       >
-        {socialStatus.map(state => (
-          <Option key={state}>{state}</Option>
+        {socialStatus.map(({ label, value }) => (
+          <Option key={value} value={value}>
+            {label}
+          </Option>
         ))}
       </Select>
       <Select
@@ -49,7 +80,7 @@ const Filters = ({ dispatch, filters, notes, meta, tags }) => {
         mode="multiple"
         placeholder="Tags"
         value={filters.tags}
-        onChange={values => setFilterValues("tags", values)}
+        onChange={values => setFilterValues({ tags: values })}
       >
         {tags.map(({ label, value, _id }) => (
           <Option key={_id} value={value}>
@@ -57,7 +88,7 @@ const Filters = ({ dispatch, filters, notes, meta, tags }) => {
           </Option>
         ))}
       </Select>
-      {meta && (
+      {meta && meta.count > 0 && (
         <span className="showingCount">
           Showing {notes.length} of {meta.count}
         </span>
