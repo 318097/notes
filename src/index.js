4bcf82c8c861f81c8670b697fbbbbb1175f238c8
Mehul Lakhanpal <318097@gmail.com>
2020-04-09 09:42:10 +0530

    Add: Redux Persist

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.scss";
 import "antd/dist/antd.css";
@@ -9,13 +10,15 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
-import store from "./store";
+import { store, persistor } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import reducer from './reducer';
+import reducer from "./reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const middlewares = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)));
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
