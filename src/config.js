bc40fa5826db88f5af8ab9697afa02d1e18632da
Mehul Lakhanpal <318097@gmail.com>
2021-04-13 17:31:23 +0530

    Update: Config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
+const isProd = process.env.REACT_APP_NODE_ENV === "production";
+
 const config = {
   LIMIT: 25,
-  SERVER_URL: process.env.REACT_APP_SERVER_URL
-    ? process.env.REACT_APP_SERVER_URL
+  SERVER_URL: isProd
+    ? "https://bubblegum-server.herokuapp.com/api"
     : "http://localhost:7000/api",
 };
 
